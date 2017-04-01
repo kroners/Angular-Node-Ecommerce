10aa88c4820f9e66ef773b1d2809907e07afa4a2
MrK <crodriguezq91@gmail.com>
2017-03-31 19:39:53 -0500

    Cambios en User Controller

--- client/controllers/user.controller.js
+++ client/controllers/user.controller.js
@@ -9,12 +9,18 @@ UserController.$inject = ['usersFactory'];
 
 function UserController(usersFactory) {
   var vm = this; // not using $scope as best practices from Jhon Papa, unless necessary
+  // consider using $scope in a controller when publishing or subscribing events
+  // using $emit, $broadcast, $on
 
 	//console.log(usersFactory);
   //initializing variables inside the Controller
   vm.user = {};
   vm.loggedUser = {};
   vm.loggedIn = false;
+  vm.loginUser = loginUser;
+  vm.registerUser = registerUser;
+  vm.logout = logout;
+  vm.toggle = toggle;
   vm.loginErrors = '';
   vm.regErrors = {
         firstName       : '',
@@ -34,9 +40,6 @@ function UserController(usersFactory) {
   //Create user
   // Connect to factory to Create user
   // According to Jhon Papa, we should defer the Controller Logic to service
-
-  vm.registerUser = registerUser;
-
   function registerUser() {
 
     //making inside Front End Validations
@@ -65,9 +68,41 @@ function UserController(usersFactory) {
       // Aun no se añaden validators, por lo que solo se asigna la data de usuario al logueado, despues de ser creado.
       vm.loggedUser = data.user;
     })
-  }
+  };
 
-  vm.logout = logout;
+  // Search if user is LoggedIn
+  function checkSessionUser () {
+    vm.userFact.isLoggedIn(function(vm.user) {
+      if(vm.user.id){
+        vm.loggedUser = user;
+        vm.loggedIn = true;
+      }
+    })
+  };
+
+  // User is Logging In
+  function loginUser () {
+    vm.loginErrors = '';
+
+    usersFactory.login($scope.loginUser, function(data) {
+      if (data) {
+        //Yes User.
+        if (!data.error) {
+          vm.loggedUser = data;
+          vm.loggedIn = true;
+
+          $('#Login').modal('toggle');
+          vm.loginUser = {};
+        } else {
+          //Bad Password.
+          vm.loginErrors = 'Failed login, please check your email and password.';
+        }
+      //No User.
+      } else{
+        vm.loginErrors = 'Failed login, please check your email and password.';
+      }
+    })
+  };
 
   function logout() {
     // Log out through Passport, then clear local user data and redirect
@@ -75,7 +110,7 @@ function UserController(usersFactory) {
       $scope.loggedUser = {};
       $scope.loggedIn = false;
     });
-  }
+  };
 
 };
 
